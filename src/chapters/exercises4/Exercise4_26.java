6d7c8769070ceace98c8c4905bfdb52308c2e6f1
safa <sfabyram@hotmail.com>
2022-09-02 20:56:54 +0300

    updated exersice4

---
+++ src/chapters/exercises4/Exercise4_26.java
@@ -0,0 +1,16 @@
+package chapters.exercises4;
+
+public class Exercise4_26 {
+	public static void main(String[] args) {
+		String s = "safabayramx";
+		System.out.println(s.charAt(s.length() - 1));
+
+		
+		
+	}
+
+	
+	
+	}
+
+
