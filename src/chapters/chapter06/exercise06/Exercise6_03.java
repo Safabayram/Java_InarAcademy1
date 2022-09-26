a5896a9bcbe256131bb863939d19bfa514dc2c73
safa <sfabyram@hotmail.com>
2022-09-26 23:37:20 +0300

    chapter06

---
+++ src/chapters/chapter06/exercise06/Exercise6_03.java
@@ -0,0 +1,31 @@
+package chapters.chapter06.exercise06;
+
+import java.util.Scanner;
+
+public class Exercise6_03 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter an integer");
+        int s = input.nextInt();
+        System.out.println(isPalindrome(s));
+
+
+    }
+    public  static int reverse(int number){
+        int reverse = 0;
+        while(number != 0){
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+        return  reverse;
+
+    }
+    public static boolean isPalindrome(int number){
+        if(number == reverse(number)){
+            return true;
+        }
+        return false;
+    }
+}
