220c6b9bef186f103a94a800c1cf5c4f93da4921
safa <sfabyram@hotmail.com>
2022-10-15 16:03:42 +0300

    exercises7

--- src/chapters/chapter07/exercises07/Exercise_15.java
+++ src/chapters/chapter07/exercises07/Exercise_15.java
@@ -1,4 +1,54 @@
 package chapters.chapter07.exercises07;
 
+import java.util.Scanner;
+
 public class Exercise_15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[10];
+
+
+        System.out.print("Enter ten number: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
+        int[] distinctNumbers = eliminateDuplicates(numbers);
+
+
+        System.out.print("The distinct numbers are:");
+        for (int a: distinctNumbers) {
+            if (a > 0)
+                System.out.print(" " + a);
+        }
+        System.out.println();
+    }
+
+    private static int[] eliminateDuplicates(int[] num) {
+        int[] distinctNumbers = new int[num.length];
+        for (int i = 0; i < num.length ; i++) {
+            if (getDublicate(distinctNumbers,i) == -1) {
+                distinctNumbers[i] = i;
+                i++;
+            }
+
+
+        }
+        return distinctNumbers;
+
+
+    }
+
+
+
+
+    private static int getDublicate(int[] distincs, int key) {
+        for (int i = 0; i <distincs.length ; i++) {
+            if (key == distincs[i])
+                return i;
+
+        }
+        return -1;
+    }
+
+
 }
