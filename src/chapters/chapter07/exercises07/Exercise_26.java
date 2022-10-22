2ee8a4c6be8157a7e77f0e9026353885ba38befd
safa <sfabyram@hotmail.com>
2022-10-22 12:46:19 +0300

    some exercises

---
+++ src/chapters/chapter07/exercises07/Exercise_26.java
@@ -0,0 +1,44 @@
+package chapters.chapter07.exercises07;
+
+import java.util.Scanner;
+
+public class Exercise_26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter list1: ");
+        int[] list1 = new int[input.nextInt()];
+
+        for (int i = 0; i < list1.length ; i++) {
+            list1[i] = input.nextInt();
+        }
+
+        System.out.print("Enter list2: ");
+        int[] list2 = new int[input.nextInt()];
+        for (int i = 0; i < list2.length; i++) {
+            list2[i] = input.nextInt();
+        }
+
+        System.out.println("Two lists are" + (equals(list1, list2) ? " " : " not ")
+                + "strictly identical");
+
+
+
+
+    }
+
+    public static boolean equals(int[] list1, int[] list2) {
+        if (list1.length != list2.length){
+            return false;
+
+        }
+        for (int i = 0; i < list1.length; i++) {
+            if (list1[i] != list2[i]){
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+
+}
