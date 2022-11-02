2fe1490a20522f370b3fc3a545a3ea787d895590
safa <sfabyram@hotmail.com>
2022-11-02 11:07:08 +0200

    chapter09

---
+++ src/chapters/chapter09/exercises09/Account.java
@@ -0,0 +1,69 @@
+package chapters.chapter09.exercises09;
+
+import java.util.Date;
+
+public class Account {
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private Date dateCreated = new Date();
+
+
+    public Account() {
+
+    }
+
+    public Account(int id, double balance, double annualInterestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+
+    }
+
+    public void setAnnualInterestRate() {
+        this.annualInterestRate = annualInterestRate;
+
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public double getMonthlyInterestRate() {
+        return getAnnualInterestRate() / 12;
+
+    }
+
+    public double getMonthlyInterest() {
+        return getBalance() * getMonthlyInterestRate();
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
+
