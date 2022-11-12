2fe85db854ecd92ce0e09a819cda0117e4e646be
safa <sfabyram@hotmail.com>
2022-11-12 16:35:17 +0200

    chapter10 exercises

--- src/chapters/chapter10/exercises10/Exercise_15.java
+++ src/chapters/chapter10/exercises10/Exercise_15.java
@@ -1,6 +1,6 @@
 package chapters.chapter10.exercises10;
 
-/*import java.util.Scanner;
+import java.util.Scanner;
 
 public class Exercise_15 {
     public static void main(String[] args) {
@@ -16,14 +16,47 @@ public class Exercise_15 {
             }
         }
         MyRectangle2D rectangle2D = getRectangle(points);
-
-
-
-
-
+        System.out.println("The bounding rectangle's center (" + rectangle2D.getX() + "," + rectangle2D.getY() + "), width "
+                + rectangle2D.getWidth() + " , " + "height " + rectangle2D.getHeight());
 
 
     }
 
+    public static MyRectangle2D getRectangle(double[][] points) {
+        double smallestx1 = points[0][0];
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] < smallestx1) {
+                smallestx1 = points[i][0];
+            }
+        }
+        double biggestx2 = points[points.length - 1][0];
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] > biggestx2) {
+                biggestx2 = points[i][0];
+            }
+        }
+        double x = (smallestx1 + biggestx2) / 2.0;
+        double width = Math.abs(biggestx2 - smallestx1);
 
-}*/
+        double smallesty1 = points[0][1];
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][1] < smallesty1) {
+                smallesty1 = points[i][1];
+            }
+        }
+        double biggesty2 = points[points.length - 1][1];
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][1] > biggesty2) {
+                biggesty2 = points[i][1];
+            }
+        }
+        double y = (smallesty1 + biggesty2) / 2.0;
+        double height = Math.abs(biggesty2 - smallesty1);
+        return new MyRectangle2D(x, y, width, height);
+    }
+
+}
+
+
+
+
--- src/chapters/chapter10/exercises10/Exercise_25.java
+++ src/chapters/chapter10/exercises10/Exercise_25.java
@@ -1,11 +1,64 @@
 package chapters.chapter10.exercises10;
 
-/*public class Exercise_25 {
+import java.util.Arrays;
+
+public class Exercise_25 {
     public static void main(String[] args) {
-
+        String[] splittedArray = split("#ab##12#453", "#");
+        for (int i = 0; i < splittedArray.length; i++) {
+            System.out.print(splittedArray[i] + " ");
+        }
+        System.out.println();
     }
+
+
     public static String[] split(String s, String regex) {
+        char[] chars = s.toCharArray();
+        char[] charRegex = getRegex(regex);
+        String[] splittedArray = new String[chars.length];
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            boolean isContainsRegex = false;
+            for (int j = 0; j < charRegex.length; j++) {
+                if (chars[i] == charRegex[j]) {
+                    isContainsRegex = true;
+                }
+
+            }
+            if (isContainsRegex) {
+                if (splittedArray[count] == null) {
+                    splittedArray[count] = "" + chars[i];
+
+                } else {
+                    count++;
+                    splittedArray[count] = "" + chars[i];
+
+
+                }
+                count++;
+            } else {
+                if (splittedArray[count] == null) {
+                    splittedArray[count] = "" + chars[i];
+                } else {
+                    splittedArray[count] = "" + chars[i];
+                }
+            }
+
+        }
+        String[] result = new String[count + 1];
+        System.arraycopy(splittedArray, 0, result, 0, result.length);
+        return result;
+
 
     }
 
-}*/
+    public static char[] getRegex(String regex) {
+        if (regex.length() == 1) {
+            return regex.toCharArray();
+        } else {
+            char[] array = regex.toCharArray();
+            return Arrays.copyOfRange(array, 1, array.length - 1);
+        }
+    }
+}
+
