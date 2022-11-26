90be7822c10c6674d2133ccb91f6988ae5d04e0b
safa <sfabyram@hotmail.com>
2022-11-26 15:30:15 +0200

    chapter12

--- src/chapters/chapter12/exercises_12/Exercise_19/Exercise_19.java
+++ src/chapters/chapter12/exercises_12/Exercise_19/Exercise_19.java
@@ -4,25 +4,23 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class Exercise_19 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
             java.net.URL myUrl = new URL("http://cs.armstrong.edu/liang/data/Lincoln.txt");
 
             int count = 0;
             Scanner input = new Scanner(myUrl.openStream());
-            while (input.hasNext()){
-                if (Character.isLetter(input.next().charAt(0))){
+            while (input.hasNext()) {
+                if (Character.isLetter(input.next().charAt(0))) {
                     count++;
 
                 }
             }
             System.out.println("number of words in adress :" + count);
-        }
-        catch (java.net.MalformedURLException ex){
+        } catch (java.net.MalformedURLException ex) {
             System.out.println(ex.getMessage());
 
-        }
-        catch (java.io.IOException ex){
+        } catch (java.io.IOException ex) {
             System.out.println(ex.getMessage());
         }
 
--- src/chapters/chapter12/exercises_12/Exercise_20/Exercise_20.java
+++ src/chapters/chapter12/exercises_12/Exercise_20/Exercise_20.java
@@ -1,13 +1,13 @@
-package chapters.chapter12.exercises_12.Exercise_18;
+package chapters.chapter12.exercises_12.Exercise_20;
 
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class Exercise_18 {
+public class Exercise_20 {
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.out.println("Usage: java Exercise12_18 srcRootDirectory");
+            System.out.println("Usage: java Exercise12_20 srcRootDirectory");
             System.exit(1);
         }
 
@@ -17,48 +17,51 @@ public class Exercise_18 {
             System.out.println("Current directory " + args[0] + " does not exist");
             System.exit(2);
         }
+
         if (!curDir.isDirectory()) {
             System.out.println(args[0] + " is not a directory");
             System.exit(3);
         } else {
             File[] files = curDir.listFiles();
-            for (int i = 0; i < files.length; i++) {
+            for (int i = 0; i < files.length; i++)
                 if (files[i].isDirectory() && files[i].getName().startsWith("chapter")) {
-                    String pack = "package" + files[i].getName() +"\\";
-                    insertStaToFile(files[i],pack);
-
+                    deleteStatementsInDirectory(files[i]);
                 }
-            }
-
-
         }
+
 
     }
 
-    private static void insertStaToFile(File directory, String line) throws Exception {
+    private static void deleteStatementsInDirectory(File directory)
+            throws Exception {
         File[] files = directory.listFiles();
 
         for (int i = 0; i < files.length; i++)
             if (files[i].isFile() && files[i].getName().endsWith(".java")) {
-                insertStatement(files[i], line);
+                deleteStatement(files[i]);
             }
     }
 
-    private static void insertStatement(File file, String line) throws Exception {
-        StringBuilder sb = new StringBuilder(line);
+    private static void deleteStatement(File file) throws Exception {
+        StringBuilder sb = new StringBuilder();
 
         try (
-                Scanner input = new Scanner(file);
-        ) {
-            while (input.hasNext())
-                sb.append("\r\n" + input.nextLine());
+                Scanner input = new Scanner(file)) {
+
+            String firstLine = input.nextLine();
+            if (!firstLine.toString().startsWith("package"))
+                sb.append(firstLine + "\r\n");
+
+            while (input.hasNext()) {
+                sb.append(input.nextLine() + "\r\n");
+            }
         }
 
         try (
-
-                PrintWriter output = new PrintWriter(file);
+              PrintWriter output = new PrintWriter(file);
         ) {
-            output.printf("%s\r\n", sb.toString());
+            output.printf("%s", sb.toString());
         }
     }
+
 }
