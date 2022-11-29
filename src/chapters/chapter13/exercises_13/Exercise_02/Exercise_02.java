be8c21eb8e5df700d84f4813965ad3faced742f1
safa <sfabyram@hotmail.com>
2022-11-29 23:04:44 +0200

    chapter13

---
+++ src/chapters/chapter13/exercises_13/Exercise_02/Exercise_02.java
@@ -0,0 +1,33 @@
+package chapters.chapter13.exercises_13.Exercise_02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Exercise_02 {
+    public static void main(String[] args) {
+        Number [] numbers = {1,2,3,4,5,6,7,8,9};
+
+        ArrayList<Number> list = new ArrayList<>(Arrays.asList(numbers));
+
+        shuffle(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+
+
+        }
+
+    }
+    public static void shuffle(ArrayList<Number> list) {
+        //Collections.shuffle(list);
+        for (int i = 0; i <list.size() ; i++) {
+            int index = (int)(Math.random() * list.size());
+            Number temp = list.get(i);
+            list.set(i, list.get(index));
+            list.set(index,temp);
+        }
+
+        }
+    }
+
---
+++ src/chapters/chapter13/exercises_13/Exercise_06/ComparableCircle.java
@@ -0,0 +1,19 @@
+package chapters.chapter13.exercises_13.Exercise_06;
+
+public class ComparableCircle extends Circle implements Comparable<ComparableCircle> {
+    public ComparableCircle() {
+    }
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius())
+            return 1;
+        else if (getRadius() < o.getRadius())
+            return -1;
+        else
+            return 0;
+    }
+
+
+}
